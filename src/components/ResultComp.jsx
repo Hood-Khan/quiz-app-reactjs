0b3cc9bb42929c7b590fcb200b6016db961ca5a4
HoodKhan <muhammadhoodkhan9366@gmail.com>
2025-12-28 23:59:25 +0500

    result logic added

--- src/components/ResultComp.jsx
+++ src/components/ResultComp.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import quiz from '../api/quiz.json'
 
 function ResultComp({showScreen,setShowScreen,answers,setAnswers}) {
 
+  const totalQuestions = quiz.length
+  const attemptQuestions = answers.length
+  const totalCorrect = answers.length>0 ? answers.filter((cur,i)=>{
+    const questions = quiz.find((c)=>c.id == cur.id)
+    return questions.correct_option === cur.answer
+  }) : []
 
-
+  const totalIncorrect = answers.length>0 ? answers.filter((cur,i)=>{
+    const questions = quiz.find((c)=>c.id == cur.id)
+    return questions.correct_option !== cur.answer
+  }) : []
+  
   return (
     <div className="w-full bg-white/30 backdrop-blur-lg 
                     border border-white/40 rounded-2xl shadow-xl 
@@ -12,18 +23,18 @@ function ResultComp({showScreen,setShowScreen,answers,setAnswers}) {
       <div className="max-w-3xl mx-auto text-center">
 
         <h1 className="text-4xl font-bold text-green-700 mb-8">
-          🎉 You Scored 10 / 10
+          🎉 You Scored {totalCorrect.length} / {totalQuestions}
         </h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-10">
           <div className="bg-white/40 rounded-xl p-6 text-lg font-medium text-gray-800">
             ✅ Correct Answers  
-            <div className="text-3xl font-bold mt-2">5</div>
+            <div className="text-3xl font-bold mt-2">{totalCorrect.length}</div>
           </div>
 
           <div className="bg-white/40 rounded-xl p-6 text-lg font-medium text-gray-800">
             ❌ Incorrect Answers  
-            <div className="text-3xl font-bold mt-2">5</div>
+            <div className="text-3xl font-bold mt-2">{totalIncorrect.length}</div>
           </div>
         </div>
 
